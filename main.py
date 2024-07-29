0a7a4383c47fda8f5274ee137d222b90fdd2d288
george udosen <datameshprojects@gmail.com>
2024-07-29 12:26:34 +0100

    third commit

---
+++ main.py
@@ -0,0 +1,236 @@
+from enum import Enum
+from typing import Annotated
+from fastapi import FastAPI, Query, Path, Body
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
+
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+    
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+    
+    
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+
+@app.get('/')
+async def root():
+    return {"message": "Hello, world!"}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+# If you declare it as is, because it is a singular value, FastAPI will assume that it is a query parameter.
+# But you can instruct FastAPI to treat it as another body key using Body
+@app.put("/items/{item_id}")
+async def update_item(
+    item_id: int, item: Item, user: User, importance: Annotated[int, Body()]
+):
+    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+    return results
+
+# @app.put("/items/{item_id}") # But you can also declare multiple body parameters, e.g. item and user
+# async def update_item(item_id: int, item: Item, user: User):
+#     results = {"item_id": item_id, "item": item, "user": user}
+#     return results
+
+
+# @app.put("/items/{item_id}") # And you can also declare body parameters as optional, by setting the default to None
+# async def update_item(
+#     item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=1000)],
+#     q: str | None = None,
+#     item: Item | None = None,
+# ):
+#     results = {"item_id": item_id}
+#     if q:
+#         results.update({"q": q})
+#     if item:
+#         results.update({"item": item})
+#     return results
+
+
+# @app.get("/items/{item_id}") # Number validations: floats, greater than and less than
+# async def read_items(
+#     *,
+#     item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=1000)],
+#     q: str,
+#     size: Annotated[float, Query(gt=0, lt=10.5)],
+# ):
+#     results = {"item_id": item_id}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+
+# @app.get("/items/{item_id}") # Number validations: greater than and less than or equal
+# async def read_items(
+#     item_id: Annotated[int, Path(title="The ID of the item to get", gt=0, le=1000)],
+#     q: str,
+# ):
+#     results = {"item_id": item_id}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+# @app.get("/items/{item_id}") # Number validations: greater than or equal
+# async def read_items(
+#     item_id: Annotated[int, Path(title="The ID of the item to get", ge=1)], q: str
+# ):
+#     results = {"item_id": item_id}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+# @app.get("/items/{item_id}") 
+# async def read_items(
+#     q: str, item_id: Annotated[int, Path(title="The ID of the item to get")]
+# ):
+#     results = {"item_id": item_id}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+# @app.get("/items/{item_id}")
+# async def read_items(q: str, item_id: int = Path(title="The ID of the item to get")):
+#     results = {"item_id": item_id}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+
+# @app.get("/items/{item_id}")
+# async def read_items(
+#     item_id: Annotated[int, Path(title="The ID of the item to get")],
+#     q: Annotated[str | None, Query(alias="item-query")] = None,
+# ):
+#     results = {"item_id": item_id}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item, q: str | None = None):
+#     result = {"item_id": item_id, **item.dict()}
+#     if q:
+#         result.update({"q": q})
+#     return result
+
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item):
+#     return {"item_id": item_id, **item.dict()}
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: str, q: str | None = None, short: bool = False):
+#     item = {"item_id": item_id}
+#     if q:
+#         item.update({"q": q})
+#     if short:
+#         item.update(
+#             {"description": "This is an amazing item that has a long description"}
+#         )
+#     return item
+
+
+# @app.get("/items/")
+# async def read_item(skip: int = 0, limit: int = 10): # http://127.0.0.1:8000/items/?skip=0&limit=10
+#     return fake_items_db[skip : skip + limit]
+
+
+@app.get("/items/")
+async def read_items(
+    q: Annotated[
+        str | None,
+        Query(
+            alias="item-query",
+            title="Query string",
+            description="Query string for the items to search in the database that have a good match",
+            min_length=3,
+            max_length=50,
+            pattern="^fixedquery$",
+            deprecated=True,
+        ),
+    ] = None,
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+# We are going to enforce that even though q is optional, whenever it is provided, its length doesn't exceed 50 characters.
+
+# Import Query and Annotated¶
+# To achieve that, first import:
+
+# Query from fastapi
+# Annotated from typing (or from typing_extensions in Python below 3.9)
+# @app.get("/items/")
+# async def read_items(q: Annotated[str | None, Query(max_length=50)] = None):
+#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
